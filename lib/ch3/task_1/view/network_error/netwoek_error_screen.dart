c262516743cb6afbb0f07001b0ba55dd464c1fcd
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-05-22 23:06:52 +0530

    21-05-24

--- lib/ch3/task_1/view/network_error/netwoek_error_screen.dart
+++ lib/ch3/task_1/view/network_error/netwoek_error_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import 'componects/mobile_data.dart';
 import 'componects/no_internet.dart';
+import 'componects/wifi_data.dart';
 
 class Network_Error_Screen extends StatelessWidget {
   const Network_Error_Screen({super.key});
@@ -14,53 +15,11 @@ class Network_Error_Screen extends StatelessWidget {
         stream: Connectivity().onConnectivityChanged,
         builder: (context, snapshot) {
           if (snapshot.data!.contains(ConnectivityResult.mobile)) {
-            // RETURN WIDGET
+            // RETURN MOBILE DATA
             return mobile_data_connection();
           } else if (snapshot.data!.contains(ConnectivityResult.wifi)) {
-            // RETURN WIDGET
-            return SafeArea(
-              child: Container(
-                height: double.infinity,
-                width: double.infinity,
-                color: const Color(0xff2dce57),
-                child: Center(
-                  child: Container(
-                    height: 350,
-                    width: 270,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: const [
-                          BoxShadow(
-                            color: Colors.grey,
-                            blurRadius: 1,
-                            spreadRadius: 1,
-                          )
-                        ],
-                        borderRadius: BorderRadius.circular(10)
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(height: 180,child: Image.asset('asset/img/NoConnection.png')),
-                        const Text(
-                          'Connected to wi-fi',
-                          style: TextStyle(fontWeight: FontWeight.w800,fontSize: 17),
-                        ),
-                        const SizedBox(height: 10,),
-                        const SizedBox(
-                          width: 280,
-                          child: Text(
-                            'Your internet successfullly connected',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(fontWeight: FontWeight.w200,fontSize: 15),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            );
+            // RETURN WIFI
+            return wifi_data();
           } else {
             // RETURN NO INTERNET CONNECTION
             return no_internet_connection();
@@ -69,5 +28,4 @@ class Network_Error_Screen extends StatelessWidget {
       ),
     );
   }
-
 }
